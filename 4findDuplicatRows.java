1b6d5a88aa1aef36535bce08a570528d31cb9ab4
Saif-Alioo <71405722+Saif-Alioo@users.noreply.github.com>
2024-01-14 17:25:57 +0530

    Create 4findDuplicatRows.java
    Given a boolean matrix of size RxC where each cell contains either 0 or 1, find the row numbers (0-based) of row which already exists or are repeated.

---
+++ 4findDuplicatRows.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    public static ArrayList<Integer> repeatedRows(int matrix[][], int m, int n)
+    {
+        //code here
+        ArrayList<Integer> ans= new ArrayList<Integer>();
+        Set<String> set= new HashSet<>();
+        for(int i=0;i<m;i++){
+            StringBuilder curr=new StringBuilder();
+            for(int j=0;j<n;j++){
+                curr.append(matrix[i][j]+" ");
+            }
+            if(set.contains(curr.toString()))ans.add(i);
+            set.add(curr.toString());
+        }
+        return ans;
+    }
+}
